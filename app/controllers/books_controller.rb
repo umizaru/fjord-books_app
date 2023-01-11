557b2cff2a90011feca75fcafc359a2b1bca675f
umizaru <y.wakita1T@gmail.com>
2023-01-11 21:31:39 +0900

    不要な文字列を削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -70,6 +70,6 @@ class BooksController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def book_params
-    params.require(:book).permit(t(:title), t(:memo), t(:author), t(:picture))
+    params.require(:book).permit(:title, :memo, :author, :picture)
   end
 end
