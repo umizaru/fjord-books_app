6fd2b672bd5a81e9bc12758ea51d960efcb742f0
umizaru <y.wakita1T@gmail.com>
2023-01-14 21:50:57 +0900

    登録画面にカラムを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.order(:id).page(params[:page]).per(2)
+    @books = Book.order(:id).page(params[:page]).per(20)
   end
 
   # GET /books/1
