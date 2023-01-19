d4884c9783666a9ad9902161776829176c29e900
umizaru <y.wakita1T@gmail.com>
2023-01-19 22:58:09 +0900

    ユーザ一覧画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :books
+  get 'users', to: 'users#index'
   # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
