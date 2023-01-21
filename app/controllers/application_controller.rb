4a399a43fea4ffcfda19bd22e51f86fd4b126f27
umizaru <y.wakita1T@gmail.com>
2023-01-21 13:50:14 +0900

    ストロングパラメータを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:introduction])
+  end
 end
--- db/migrate/20230114121251_add_registration_to_users.rb
+++ db/migrate/20230114121251_add_registration_to_users.rb
@@ -1,6 +1,6 @@
 class AddRegistrationToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :nickname, :string
+    add_column :users, :name, :string
     add_column :users, :postal_code, :string
     add_column :users, :address, :string
     add_column :users, :introduction, :text
