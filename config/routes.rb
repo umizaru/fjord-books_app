f589d1d6b5a5c04cffcb9ddd4ba26006e67a1cac
umizaru <y.wakita1T@gmail.com>
2023-01-23 22:24:19 +0900

    メール送信設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :books
   resources :users, only: [:index, :show]
   root to: 'books#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
