09418eb2a4af5efae1cff45bddf9f347abb113fa
umizaru <y.wakita1T@gmail.com>
2023-01-24 18:33:22 +0900

    rubocop,erblinをパス

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
     @users = User.order(:id).page(params[:page]).per(4)
@@ -7,5 +10,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
 end
