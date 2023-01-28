66a4e08031a7bdfd571e2d657fab03ffdd8de88e
umizaru <y.wakita1T@gmail.com>
2023-01-28 10:22:50 +0900

    コントローラーにアクションを定義

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,14 @@
+class RelationshipsController < ApplicationController
+
+  def create
+    user = User.find(params[:follow_id])
+    current_user.follow(params[:follow_id])
+    record_to user_path(user)
+  end
+
+  def destroy
+    Relationship.find_by(follower_id: current_user.id, following_id: params[:id]).destroy
+    redirect_to user_path
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: %I[show edit update destroy followings followers]
+
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
@@ -8,4 +11,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def followings
+    @followings = @user.following_users
+  end
+
+  def followers
+    @followers = @user.follower_users
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
+
