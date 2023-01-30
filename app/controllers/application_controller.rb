a678e6b561426d3b55343d06accb75e3deb2b1a3
umizaru <y.wakita1T@gmail.com>
2023-01-30 22:29:46 +0900

    不要なファイルを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:introduction])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:address])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :address, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :address, :introduction])
   end
 end
