6af0ae75a1a7012ceedb841b2b41528c505c3c61
umizaru <y.wakita1T@gmail.com>
2023-02-04 20:14:19 +0900

    フォロー/フォロワー逆転問題を解消

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,12 @@
 class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:follow_id])
-    current_user.follow(params[:follow_id])
+    current_user.follow(user.id)
     redirect_to user_path(user)
   end
 
   def destroy
-    current_user.followers.find_by!(following_id: params[:id]).destroy
+    current_user.followings.find_by!(follower_id: params[:id]).destroy
     redirect_to user_path
   end
 end
-aw
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,22 +6,23 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :followers,
-           class_name: 'Relationship',
-           foreign_key: 'follower_id',
-           dependent: :destroy,
-           inverse_of: :follower
   has_many :followings,
            class_name: 'Relationship',
            foreign_key: 'following_id',
            dependent: :destroy,
            inverse_of: :following
 
-  has_many :following_users, through: :followers, source: :following
-  has_many :follower_users, through: :followings, source: :follower
+  has_many :followers,
+           class_name: 'Relationship',
+           foreign_key: 'follower_id',
+           dependent: :destroy,
+           inverse_of: :follower
+
+  has_many :following_users, through: :followings, source: :follower
+  has_many :follower_users, through: :followers, source: :following
 
   def follow(other_user_id)
-    followers.create(following_id: other_user_id)
+    followings.create(follower_id: other_user_id)
   end
 
   def following?(other_user)
@@ -29,6 +30,6 @@ class User < ApplicationRecord
   end
 
   def unfollow(other_user_id)
-    followers.destroy(following_id: other_user_id)
+    followings.destroy(following_id: other_user_id)
   end
 end
