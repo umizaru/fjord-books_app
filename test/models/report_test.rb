eca8c07f019e01d84f152c1f1ec8ffe3e3b2add0
umizaru <y.wakita1T@gmail.com>
2023-02-19 08:39:25 +0900

    user_idの取得方法を修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,13 +4,13 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test 'editable?' do
-    report = Report.create!(user_id: User.first.id, title: 'サンプルな件名', content: 'サンプルな内容')
-    user = User.first
+    report = Report.create!(user_id: users(:alice).id, title: 'サンプルな件名', content: 'サンプルな内容')
+    user = users(:alice)
     assert report.editable?(user)
   end
 
   test 'created_on?' do
-    report = Report.create!(user_id: User.first.id, title: 'サンプルな件名', content: 'サンプルな内容')
+    report = Report.create!(user_id: users(:alice).id, title: 'サンプルな件名', content: 'サンプルな内容')
     assert_equal report.created_at.to_date, report.created_on
   end
 end
